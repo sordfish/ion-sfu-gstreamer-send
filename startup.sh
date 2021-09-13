5fc38b7432137884abc93c795c16b16cf8432a4b
sordfish <sordfish2050@gmail.com>
2021-09-13 16:38:51 +0100

    use env vars for config

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "Starting Listener on port 9000 for session 'test session'"
-/ion-sfu-gstreamer-send -addr "localhost:50051" -session "test session" -video-src "udpsrc port=9000 ! h264parse ! rtph264pay config-interval=10 pt=96 ! rtph264depay ! avdec_h264 ! videoconvert"
+echo "Starting Listener on port $ION_CLIENT_SFU_ADDR for session $ION_CLIENT_SFU_SESSION"
+./ion-sfu-gstreamer-send -addr $ION_CLIENT_SFU_ADDR -session $ION_CLIENT_SFU_SESSION -video-src $ION_CLIENT_VIDEO_SRC
