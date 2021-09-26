786147f2a7907f5a19ef1637f80897ff4a2e0858
Arran Short <sordfish2050@gmail.com>
2021-09-26 16:12:33 +0100

    shellcheck fixed the loader error not passing the full command

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "Starting Listener on port $ION_CLIENT_SFU_ADDR for session $ION_CLIENT_SFU_SESSION"
-./ion-sfu-gstreamer-send -addr $ION_CLIENT_SFU_ADDR -session $ION_CLIENT_SFU_SESSION -video-src $ION_CLIENT_VIDEO_SRC
+echo "./ion-sfu-gstreamer-send -addr $ION_CLIENT_SFU_ADDR -session $ION_CLIENT_SFU_SESSION -video-src \"$ION_CLIENT_VIDEO_SRC\" "
+./ion-sfu-gstreamer-send -addr "$ION_CLIENT_SFU_ADDR" -session "$ION_CLIENT_SFU_SESSION" -video-src \""$ION_CLIENT_VIDEO_SRC"\"
