05a41b3ad2a50a886bfb9d7608268806dc233ccc
sordfish <sordfish2050@gmail.com>
2021-09-29 13:22:55 +0100

    add flag for video codec

--- main.go
+++ main.go
@@ -16,9 +16,10 @@ var (
 
 func main() {
 	// parse flag
-	var session, addr string
+	var session, addr, videocodec string
 	flag.StringVar(&addr, "addr", "localhost:50051", "Ion-sfu grpc addr")
 	flag.StringVar(&session, "session", "test room", "join session name")
+	flag.StringVar(&videocodec, "video-codec", "vp8", "set video codec vp8 or h264")
 	audioSrc := flag.String("audio-src", "audiotestsrc", "GStreamer audio src")
 	videoSrc := flag.String("video-src", "videotestsrc", "GStreamer video src")
 	flag.Parse()
@@ -59,9 +60,24 @@ func main() {
 		return
 	}
 
-	videoTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "video/h264"}, "video", "pion2")
-	if err != nil {
-		panic(err)
+	var videoTrack *webrtc.TrackLocalStaticSample
+
+	switch videocodec {
+	case "vp8":
+		videoTrack, err = webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "video/vp8"}, "video", "pion2")
+		if err != nil {
+			panic(err)
+		}
+	case "h264":
+		videoTrack, err = webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "video/h264"}, "video", "pion2")
+		if err != nil {
+			panic(err)
+		}
+	default:
+		videoTrack, err = webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "video/vp8"}, "video", "pion2")
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	_, err = peerConnection.AddTrack(videoTrack)
@@ -88,7 +104,16 @@ func main() {
 
 	// Start pushing buffers on these tracks
 	gst.CreatePipeline("opus", []*webrtc.TrackLocalStaticSample{audioTrack}, *audioSrc).Start()
-	gst.CreatePipeline("h264", []*webrtc.TrackLocalStaticSample{videoTrack}, *videoSrc).Start()
+
+	switch videocodec {
+	case "vp8":
+		gst.CreatePipeline("vp8", []*webrtc.TrackLocalStaticSample{videoTrack}, *videoSrc).Start()
+	case "h264":
+		gst.CreatePipeline("h264", []*webrtc.TrackLocalStaticSample{videoTrack}, *videoSrc).Start()
+	default:
+		gst.CreatePipeline("vp8", []*webrtc.TrackLocalStaticSample{videoTrack}, *videoSrc).Start()
+
+	}
 
 	select {}
 }
