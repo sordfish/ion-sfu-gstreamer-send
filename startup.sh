b00ae4c7ef78c141d00f75f005ee2a0525048f8f
sordfish <sordfish2050@gmail.com>
2021-10-11 11:03:07 +0100

    tidy up gh-pages repo?

--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Starting Listener on port $ION_CLIENT_SFU_ADDR for session $ION_CLIENT_SFU_SESSION"
-echo "./ion-sfu-gstreamer-send -addr $ION_CLIENT_SFU_ADDR -session $ION_CLIENT_SFU_SESSION -video-src \"$ION_CLIENT_VIDEO_SRC\" "
-./ion-sfu-gstreamer-send -addr "$ION_CLIENT_SFU_ADDR" -session "$ION_CLIENT_SFU_SESSION" -video-codec "vp8" -video-src \""$ION_CLIENT_VIDEO_SRC"\"
